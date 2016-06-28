1bcf6a1d226ad747e10c10c298da4f2c6b3c3812
Jennifer Atherton <jenatherton03@gmail.com>
2016-06-28 16:33:43 +0100

    Modifying code for ex1

--- ex01.py
+++ ex01.py
@@ -1,22 +1,46 @@
 #exercise 01
-
 import datetime
 
-def inputting:
-    in_name=input("Please enter your name:")
-    if not in_name isalpha:
-        print("Please enter only letters when inputting your name")
+def questions():
 
-    in_age=input("Please enter your age:")
-    if not in_age isnumeric:
-        print("Please enter only a number when inputting your age")
+    while True:
+        name=input("Please enter your name: ")
+        name_trimmed=name.replace(" ","")
+        if not name_trimmed.isalpha():
+            print("Please enter only letters for your name")
+            #better try again... Return to the start of the loop
+            continue
+        else:
+            #age was successfully parsed!
+            #we're ready to exit the loop.
+            break
 
-    in_copies=input("Please enter a number between 1 and 10:")
-    if in_copies>10 or in_copies<1:
-        print("Please only enter a number between 1 and 10")
+    while True:
+        try:
+            age=int(input("Please enter your age: "))
+        except ValueError:
+            print("Please enter only integers for your age")
+            continue
+        else:
+            break
 
-    birth=
+    now=datetime.datetime.now()
+    nowyear=now.year
+    year_100=nowyear-age+100
 
+    while True:
+        count=int(input("Please enter a number between 1 and 10: "))
+        if int(count)>10 or int(count)<1:
+            print("Please enter only an integer")
+            continue
+        else:
+            break
 
-def to_print(in_name, in_age, in_copies):
-    print("Hello ",in_name,". You will turn 100 in the year",)
+    for i in range(count):
+        if year_100<=now.year:
+            print("Hello ",name,". You turned 100 in ",year_100,". Congratulations!", sep="")
+        else:
+            print("Hello ",name,". You will turn 100 in the year ",year_100,".",sep="")
+#
+if __name__=='__main__':
+    questions()

db97c5722c7607826383bbbebe1abe94a72776e5
Jennifer Atherton <jenatherton03@gmail.com>
2016-06-28 15:38:08 +0100

    Exercise 1 beginning

---
+++ ex01.py
@@ -0,0 +1,22 @@
+#exercise 01
+
+import datetime
+
+def inputting:
+    in_name=input("Please enter your name:")
+    if not in_name isalpha:
+        print("Please enter only letters when inputting your name")
+
+    in_age=input("Please enter your age:")
+    if not in_age isnumeric:
+        print("Please enter only a number when inputting your age")
+
+    in_copies=input("Please enter a number between 1 and 10:")
+    if in_copies>10 or in_copies<1:
+        print("Please only enter a number between 1 and 10")
+
+    birth=
+
+
+def to_print(in_name, in_age, in_copies):
+    print("Hello ",in_name,". You will turn 100 in the year",)

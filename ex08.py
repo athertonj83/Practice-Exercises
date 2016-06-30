78b9927b97e40ab34a42142239458eda07feacf5
Jennifer Atherton <jenatherton03@gmail.com>
2016-06-30 17:27:56 +0100

    Finishing RPS 2-player

--- ex08.py
+++ ex08.py
@@ -1,15 +1,69 @@
-#rps 2 player
+#rps 2-player
 
+# Defines the accepted inputs available to the players
 accepted_strings={'rock','paper','scissors'}
-player1_b=""
-player2_b=""
 
-while player1_b in accepted_strings:
+# Multiple ways of doing the while loop
+
+# 1st -Player 1 input using "while True"
+# while True:
+#     player1=input("Player 1: Please type Rock, Paper, or Scissors:")
+#     player1_a=player1.lower()
+#     player1_b=player1_a.replace(" ","")
+#     if player1_b in accepted_strings:
+#         break
+#     else:
+#         continue
+
+# 2nd - Using "while player1_a"
+player1=input("Player 1: Please type Rock, Paper, or Scissors:")
+player1_=player1.lower().replace(" ","")
+while player1_ not in accepted_strings:
     player1=input("Player 1: Please type Rock, Paper, or Scissors:")
-    player1_a=player1.lower()
-    player1_b=player1_a.replace(" ","")
+    player1_=player1.lower().replace(" ","")
 
-while player2_b in accepted_strings:
+player2=input("Player 2: Please type Rock, Paper, or Scissors:")
+player2_=player2.lower().replace(" ","")
+while player2_ not in accepted_strings:
     player2=input("Player 2: Please type Rock, Paper, or Scissors:")
-    player2_a=player2.lower()
-    player2_b=player2_a.replace(" ","")
+    player2_=player2.lower().replace(" ","")
+
+
+def game(player1_,player2_):
+
+    if player1_==player2_:
+        print("This is a tie!")
+
+    elif player1_=="rock":
+        if player2_=="scissors":
+            print("Player 1 wins with",player1_)
+        elif player2_=="paper":
+            print("Player 2 wins with",player2_)
+        else:
+            print("Check this: player1:",player1_,"player2:",player2_)
+
+    elif player1_=="scissors":
+        if player2_=="rock":
+            print("Player 2 wins with",player2_)
+        elif player2_=="paper":
+            print("Player 1 wins with",player1_)
+        else:
+            print("Check this: player1:",player1_,"player2:",player2_)
+
+    elif player1_=="paper":
+        if player2_=="rock":
+            print("Player 1 wins with",player1_)
+        elif player2_=="scissors":
+            print("Player 2 wins with",player2_)
+        else:
+            print("Check this: player1:",player1_,"player2:",player2_)
+
+    else:
+        print("Check what has happened here")
+        print("Player1:",player1)
+        print("Player2:",player2)
+
+    
+
+if __name__=='__main__':
+    game(player1_,player2_)

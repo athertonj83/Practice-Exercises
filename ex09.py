f0d94022df386fcd48b530b3e36b93b05047c73e
Jennifer Atherton <jenatherton03@gmail.com>
2016-07-01 14:26:14 +0100

    Finishing ex09

--- ex09.py
+++ ex09.py
@@ -3,26 +3,26 @@ import random
 
 #random number between 1-9 (inclusive)
 num=random.randrange(1,10)
-count=1
+count=0
+guess=0
 
-guess=input("Please guess a number between 1 and 9: ")
+while int(guess)!=num:
+    guess=input("Please guess a number between 1 and 9: ")
+    count+=1
 
-while ((guess.isdigit() and guess!=num) or (guess=="exit")):
-
-    if not guess.isdigit() and guess=="exit":
+    if guess=="exit":
         print("Quitting? Ok. The number was:",num)
         break
 
     if int(guess)==num:
         print("Well done, you guessed correct!")
         print("Number of guesses:",count)
-        break
 
     elif int(guess)<num:
         print("Too low, try again.")
-    else:
+        continue
+    elif int(guess)>num:
         print("Too high, try again.")
-
-    guess=input("Please guess another number between 1 and 9: ")
-    count+=1
-    continue
+        continue
+    else:
+        print("Second continue")
